2bc9570668a0f6dd71fe419be242ca5f1a7d2ab2
lisolavka <fenechechek@gmail.com>
2016-12-21 17:06:15 +0200

    Add files via upload

---
+++ send.php
@@ -0,0 +1,19 @@
+﻿<?
+if((isset($_POST['userName'])&&$_POST['userName']!="")&&(isset($_POST['userEmail'])&&$_POST['userEmail']!="")){ 
+        $to = 'dem.tana@gmail.com'; 
+        $subject = 'Лисолавка';
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['userName'].'</p>
+                        <p>Email: '.$_POST['userEmail'].'</p>                        
+                    </body>
+                </html>'; 
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n";
+        $headers .= "From: Отправитель <fenechechek@gmail.com>\r\n";
+        mail($to, $subject, $message, $headers);
+}
+?>
